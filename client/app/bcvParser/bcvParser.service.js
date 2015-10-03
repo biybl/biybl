e0cdc29e123efb26bbdbf4098fe4d18570923c43
Gervase Markham <gerv@gerv.net>
2015-10-03 14:50:52 +0100

    Add verse formatter

--- client/app/bcvParser/bcvParser.service.js
+++ client/app/bcvParser/bcvParser.service.js
@@ -42,25 +42,35 @@ angular.module('biyblApp')
 
           // Dispatch async request for any missing Bible texts
           for (var i = 0; i < self.passages.length; i++) {
-            if (self.passages[i]['text'] == "") {
-              var ref = self.passages[i]['ref'];
+            var passage = self.passages[i];
+
+            if (passage['text'] == "") {
+              var ref = passage['ref'];
               // Dispatch request for text (always in English for now)
               var promise = dbpGrabber.osiRangeToVerse(ref, 'en');
 
               // Handle returned text
               promise.then(function(response) {
+                // Need to search for passage again because it may have moved
+                // or disappeared when the callback arrives
                 for (var j = 0; j < self.passages.length; j++) {
                   if (self.passages[j]['ref'] == ref) {
+                    text = self.format_verses(response);
+
                     // Update the output value
-                    self.passages[j] = response;
+                    response['text'] = text;
+                    self.passages[j] = {
+                      'ref': ref,
+                      'text': text,
+                    };
 
                     // Store the returned text for later
-                    self.texts[ref] = response['text'];
+                    self.texts[ref] = text;
                   }
                 }
               }).catch(function(e) {
                 // Some error occurred
-                self.passages[j]['ref'] = "Unable to obtain Bible text";
+                passage['text'] = "Unable to obtain Bible text";
               })
             }
           }
@@ -68,6 +78,46 @@ angular.module('biyblApp')
         } else {
           if (callback) callback();
         }
+      },
+
+      format_verses: function(verses) {
+        // Ideally, all this would use templates...
+        //
+        // 'verses' is an array of verse objects from DBP API
+        //
+        // Verse object members:
+        //  book_name: localized book name (e.g. "Apocalypse")
+        //  book_id: OSIS book ID (e.g. "Rev")
+        //  chapter_id: number of chapter (e.g. "1")
+        //  chapter_title: non-localized chapter name (e.g. "Chapter 1")
+        //  verse_id: number of verse (e.g. "2")
+        //  verse_text: Text of the verse
+        //  paragraph_number: Number of paragraph; add para break when increments
+        var output = "";
+        output = output + "<h2>" + verses[0]['book_name'] + " "
+                        + verses[0]['chapter_id'] + "</h2>\n\n";
+        output = output + "<p>\n";
+
+        var current_para = verses[0]['paragraph_number'];
+        for (var i = 0; i < verses.length; i++) {
+          var verse = verses[i];
+
+          // Deal with paragraph increments
+          if (verse['paragraph_number'] != current_para) {
+            output = output + "</p>\n<p>\n";
+            current_para = verse['paragraph_number'];
+          }
+
+          // Need to deal with chapter increments here when we support them
+
+          output = output + "<span class='verseno'>" + verse['verse_id'];
+          output = output + "</span><span class='verse'>" + verse['verse_text'];
+          output = output + "</span>\n"
+        }
+
+        output = output + "\n</p>";
+
+        return output;
       },
     }
   });
