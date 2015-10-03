138188d3c961c4ee667d665f11d6b1d462e4858c
Gervase Markham <gerv@gerv.net>
2015-10-03 12:12:12 +0100

    Remove .tmp directory from repo

--- .tmp/app/admin/admin.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('biyblApp').config(function ($routeProvider) {
-  $routeProvider.when('/admin', {
-    templateUrl: 'app/admin/admin.html',
-    controller: 'AdminCtrl'
-  });
-});
-//# sourceMappingURL=admin.js.map
--- .tmp/app/main/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('biyblApp').config(function ($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'app/main/main.html',
-    controller: 'MainCtrl'
-  });
-});
-//# sourceMappingURL=main.js.map
