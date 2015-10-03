063d3df25e2cf0ca12f4b1ba56efc4bce7288114
Ben Drury <ben@cogiva.com>
2015-10-03 16:42:38 +0100

    church filter on homepage

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -3,7 +3,8 @@
 angular.module('biyblApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
     $scope.churches = [];
-
+    $scope.searchKeyword = "";
+    
     $http.get('/api/users/all').success(function(churches) {
       $scope.churches = churches;
       console.log("MainCtrl/loading/$scope.churches", $scope.churches);
