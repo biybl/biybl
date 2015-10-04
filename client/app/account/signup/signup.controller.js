a7df929bbe4298f7efe184f4ec9f80734e47ff29
Ben Drury <ben@cogiva.com>
2015-10-04 09:52:18 +0100

    Bug fix Sunday AM

--- client/app/account/signup/signup.controller.js
+++ client/app/account/signup/signup.controller.js
@@ -1,9 +1,44 @@
 'use strict';
 
 angular.module('biyblApp')
-  .controller('SignupCtrl', function ($scope, Auth, $location) {
-    $scope.user = {};
+  .controller('SignupCtrl', function ($scope, Auth, $location, $timeout) {
+    
+    $scope.user = {
+      church_name: "",
+      name: "", 
+      email: "",
+      password: ""
+    };
     $scope.errors = {};
+    $scope.checking_name = false;
+    $scope.un_ok = false;
+
+    var un_timer;
+    var cname_timer;
+
+    $scope.fill_un = function() {
+      // create slug
+      $scope.user.name = $scope.user.church_name.replace(/\W/g, '');
+      //$scope.check_name();
+    };
+
+    $scope.un_change = function() {
+      //console.log("un_change");
+      //console.log("Timeout", un_timer);
+      //$timeout.cancel(un_timer);
+      //console.log("Timeout", un_timer);
+      //un_timer = $timeout($scope.check_name(),1000);
+    };
+
+    $scope.check_name = function() {
+      //check with DB
+      //update if necessary      
+      //console.log("im in");
+      //$scope.checking_name = true;
+      //$timeout.cancel(cname_timer);
+      //cname_timer = $timeout(function(){$scope.checking_name = false;},1000);
+      //$scope.ok = true;
+    };
 
     $scope.register = function(form) {
       $scope.submitted = true;
--- client/app/verses/verses.controller.js
+++ client/app/verses/verses.controller.js
@@ -1,15 +1,48 @@
 'use strict';
 
 angular.module('biyblApp')
-  .controller('VersesCtrl', function ($scope, bcvParser, $http, User, $cookieStore) {
+  .controller('VersesCtrl', function ($scope, bcvParser, $http, User, $cookieStore, $timeout) {
     
     var currentUser = {};
     if($cookieStore.get('token')) {
-      currentUser = User.get();
+      currentUser = User.get(function(){
+        console.log("in callback");
+        console.log("name", currentUser.name);
+        $scope.church = currentUser.name;
+        $scope.getChurch(function() {
+            console.log("in get_passages");
+            bcvParser.set_refs($scope.refs, 'en', true);
+            console.log("Passages", bcvParser.passages);
+        });        
+      });
     }
 
     $scope.bcvParser = bcvParser;
     $scope.ref_list = "";
+    $scope.saving_refs = false;
+    $scope.refs_saved = false;
+    $scope.refs = [];
+    //$scope.church = 'citychurch';
+
+    $scope.getChurch = function(callback) {
+        User.getName({ id: $scope.church }, {
+        }, function(user) {
+            console.log(user);
+            $scope.church_name = user.church_name;
+            $scope.refs = user.passages.split(',');
+            $scope.ref_list = $scope.refs.join(',');
+            if (callback) callback();
+        }, function(err) {
+            if (callback) callback();
+            console.log(err);
+        });
+    }
+
+    /*$scope.getChurch(function() {
+        console.log("in get_passages");
+        bcvParser.set_refs($scope.refs, 'en', true);
+        console.log("Passages", bcvParser.passages);
+    });*/
 
     $scope.get_refs = function() {
     	console.log("VersesCtrl/get_refs/$scope.ref_list at start", $scope.ref_list);
@@ -20,6 +53,9 @@ angular.module('biyblApp')
 
     $scope.save_refs = function() {
         // Create list
+        $scope.refs_saved = false;
+        $scope.saving_refs = true;
+
         var cs_list = "";
         var first_time = true;
         for (var i=0; i<bcvParser.passages.length;i++) {
@@ -36,6 +72,8 @@ angular.module('biyblApp')
         User.savePassage({ id: currentUser._id }, {
           passages: cs_list
         }, function(user) {
+            $scope.saving_refs = false;
+            $scope.refs_saved = true;
             console.log(user);
         }, function(err) {
             console.log(err);
